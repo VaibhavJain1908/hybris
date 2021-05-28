46b6f3ff395b407a5d487f4acbf73fac863129bf
Vaibhav Jain <vaibhav.jain03@nagarro.com>
2021-05-28 11:33:26 +0530

    custom

---
+++ custom/training/trainingbackoffice/gensrc/org/trainingbackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 27-May-2021, 10:32:40 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.trainingbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "trainingbackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ custom/training/trainingfacades/gensrc/de/hybris/training/facades/constants/GeneratedTrainingFacadesConstants.java
@@ -0,0 +1,26 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 27-May-2021, 10:32:40 PM                    ---
+ * ----------------------------------------------------------------
+ *  
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.training.facades.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingFacadesConstants
+{
+	public static final String EXTENSIONNAME = "trainingfacades";
+	
+	protected GeneratedTrainingFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
